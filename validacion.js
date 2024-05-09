6456af7195a50cef91c2af76bee8a211b95627f4
Leonardo <leon102001hotmail.es>
2024-05-09 14:24:45 -0600

    Validacion de datos y envio de mail

--- validacion.js
+++ validacion.js
@@ -1,22 +1,61 @@
-function IntentoContactar(){
-    nombre=document.querySelector("form.formcontato__form input[name='nombre']").value;
-    correo=document.querySelector("form.formcontato__form input[name='email']").value;
-    asunto=document.querySelector("form.formcontato__form input[name='asunto']").value;
-    mensaje=document.getElementById("mensagem");
-    
-    if (nombre==""){
-        document.querySelector(".formcontato__botao").setAttribute('background-color','red');    
-        document.querySelector("form.formcontato__form input[name='nombre']").setAttribute('border-color','red');
-    } else if (correo==""){
-        document.querySelector(".formcontato__botao").setAttribute('background-color','red');    
-        document.querySelector("form.formcontato__form input[name='email']").setAttribute('border-color','red');
-    } else  if (asunto==""){
-        document.querySelector(".formcontato__botao").setAttribute('background-color','red');    
-        document.querySelector("form.formcontato__form input[name='asunto']").setAttribute('border-color','red'); 
-    } else if (mensaje==""){
-        document.querySelector(".formcontato__botao").setAttribute('background-color','red');    
-        document.getElementById("mensagem").setAttribute('border-color','red'); 
-    } else {
-        
+function ValidarNombre(){
+    let nombre=document.getElementById("nomb").value;
+    let mensajeEr=document.getElementById("mensajeError");
+    if (nombre===""){
+        document.getElementById("botonenv").setAttribute('disabled','true');
+        mensajeEr.innerHTML='El campo nombre esta vacio'
+    } else{
+        document.getElementById("botonenv").removeAttribute('disabled');
+        mensajeEr.innerHTML=''
+    }
+}
+
+function ValidarEmail(){
+    let email=document.getElementById("emai").value;
+    let mensajeEr=document.getElementById("mensajeError");
+    if (email===""){
+        document.getElementById("botonenv").setAttribute('disabled','true');
+        mensajeEr.innerHTML='El campo email esta vacio'
+    } else{
+        document.getElementById("botonenv").removeAttribute('disabled');
+        mensajeEr.innerHTML=''
+    }
+}
+
+function ValidarAsunto(){
+    let asunto=document.getElementById("asunt").value;
+    let mensajeEr=document.getElementById("mensajeError");
+    if (asunto===""){
+        document.getElementById("botonenv").setAttribute('disabled','true');
+        mensajeEr.innerHTML='El campo asunto esta vacio'
+    } else{
+        document.getElementById("botonenv").removeAttribute('disabled');
+        mensajeEr.innerHTML=''
+    }
+}
+
+function ValidarMensaje(){
+    let mensaje=document.getElementById("mensagem").value;
+    let mensajeEr=document.getElementById("mensajeError");
+
+    if (mensaje===""){
+        document.getElementById("botonenv").setAttribute('disabled','true');
+        mensajeEr.innerHTML='El campo mensaje esta vacio';
+    } else{
+        document.getElementById("botonenv").removeAttribute('disabled');
+        mensajeEr.innerHTML='';
+    }
+}
+
+function validaciónBoton(){
+    let nombre=document.getElementById("nomb").value;
+    let email=document.getElementById("emai").value;
+    let asunto=document.getElementById("asunt").value;
+    let mensaje=document.getElementById("mensagem").value;
+    let mensajeEr=document.getElementById("mensajeError");
+
+    if ((nombre==="")||(email==="")||(asunto==="")||(mensaje==="")){
+        document.getElementById("botonenv").setAttribute('disabled','true');
+        mensajeEr.innerHTML='Todos los campos deben etar llenos para poder enviar el correo' 
     }
 }
